5b574a72741f42ae80fa2b4d2d8b320efe1c2705
xWafl <73sampleperson@gmail.com>
2019-01-18 06:48:13 -0500

    updated 6x6 lboards

--- bot.js
+++ bot.js
@@ -151,7 +151,7 @@ client.on('message', msg => {
         } else if (msg.content.startsWith(`${prefix}leaderboards 5x5`)) {
             msg.channel.send('1. ZManGames - 8.100\n2. David Jiang - 9.002\n3. Dawid Wojcik - 9.226\n4. no name guy - 11.772\n5. Carykh - 12.346')
         } else if (msg.content.startsWith(`${prefix}leaderboards 6x6`)) {
-            msg.channel.send('1. ZManGames - 18.400\n2. Dawid Wojcik - 21.592\n3. David Jiang - 22.239\n4. Walker Welch - 26.462\n5. Carykh - 26.864')
+            msg.channel.send('1. ZManGames - 18.400\n2. Dawid Wojcik - 21.592\n3. David Jiang - 22.239\n4. no name guy - 22.635\n 5.Walker Welch - 26.462')
         } else {
             msg.channel.send('This category does not exist! (yet)');
         }
