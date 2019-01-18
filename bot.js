c96c39978acdbe6833962b138f761db6e0280006
xWafl <73sampleperson@gmail.com>
2019-01-17 19:20:37 -0500

    only submission channel can submit

--- bot.js
+++ bot.js
@@ -147,6 +147,7 @@ client.on('message', msg => {
         }
     }
     if (msg.content.startsWith(`${prefix}submit`)) {
+        if (msg.channel.id != '535613677139787777') {return;}
         let aftermessage = msg.content.slice(7);
         let aftermessageSplit = aftermessage.split(' ');
         aftermessageSplit.shift();
